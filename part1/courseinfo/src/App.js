1b03b22b4279434ffe0f1ad139a24f5d59059458
Rian Alves <rianalvesleal@gmail.com>
2025-10-23 00:37:56 -0300

    Exercises 1.1 and 1.2 done!

---
+++ part1/courseinfo/src/App.js
@@ -0,0 +1,62 @@
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  );
+};
+
+const Part = (props) => {
+  return (
+    <div>
+      <p>
+        {props.part}, exercises: {props.exercise}
+      </p>
+    </div>
+  );
+};
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part part={props.part1} exercise={props.exercise1} />
+      <Part part={props.part2} exercise={props.exercise2} />
+      <Part part={props.part3} exercise={props.exercise3} />
+    </div>
+  );
+};
+
+const Total = (props) => {
+  return (
+    <div>
+      <p>Number of exercises {props.total}</p>
+    </div>
+  );
+};
+
+const App = () => {
+  const course = "Desenvolvimento de aplicação Half Stack";
+  const part1 = "Fundamentos da biblioteca React";
+  const exercises1 = 10;
+  const part2 = "Usando props para passar dados";
+  const exercises2 = 7;
+  const part3 = "Estado de um componente";
+  const exercises3 = 14;
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content
+        part1={part1}
+        part2={part2}
+        part3={part3}
+        exercise1={exercises1}
+        exercise2={exercises2}
+        exercise3={exercises3}
+      />
+      <Total total={exercises1 + exercises2 + exercises3} />
+    </div>
+  );
+};
+
+export default App;
