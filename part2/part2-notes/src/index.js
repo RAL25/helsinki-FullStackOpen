1ac0065769dd003dfabde7d11a84eae9fb95542c
Rian Alves <rianalvesleal@gmail.com>
2026-01-16 15:34:04 -0300

    Exercises 2.11 done!

--- part2/part2-notes/src/index.js
+++ part2/part2-notes/src/index.js
@@ -1,25 +1,31 @@
-import ReactDOM from "react-dom/client"
+import ReactDOM from 'react-dom/client'
 
-import App from "./App"
+import App from './App'
 
-const notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    important: true,
-  },
-  {
-    id: 2,
-    content: "Browser can execute only JavaScript",
-    important: false,
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true,
-  },
-]
+import axios from 'axios'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <App notes={notes} />
-)
+const promise = axios.get('http://localhost:3001/notes')
+console.log(promise)
+
+const promise2 = axios.get('http://localhost:3001/foobar')
+console.log(promise2)
+
+// const notes = [
+//   {
+//     id: 1,
+//     content: 'HTML is easy',
+//     important: true,
+//   },
+//   {
+//     id: 2,
+//     content: 'Browser can execute only JavaScript',
+//     important: false,
+//   },
+//   {
+//     id: 3,
+//     content: 'GET and POST are the most important methods of HTTP protocol',
+//     important: true,
+//   },
+// ]
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
